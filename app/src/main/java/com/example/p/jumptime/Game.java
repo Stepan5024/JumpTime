c3f9946589238f310b707fcb9670ba167d62d1c7
Stepan5024 <bokarevstepan1@gmail.com>
2019-02-20 16:51:41 +0300

    Настройка

--- app/src/main/java/com/example/p/jumptime/Game.java
+++ app/src/main/java/com/example/p/jumptime/Game.java
@@ -7,15 +7,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 
 public class Game extends AppCompatActivity {
 
     private DatabaseReference myRef;
+    private FirebaseAuth mAuth;
+    private List<String> DiscTacks;
 
     ListView ListUserTask;
 
@@ -29,10 +40,34 @@ public class Game extends AppCompatActivity {
         View view = inflater.inflate(R.layout.fragment_game, container, false);
 
         ListUserTask = (ListView) findViewById(R.id.list_for_task);
-        myRef =   FirebaseDatabase.getInstance().getReference();
+        myRef = FirebaseDatabase.getInstance().getReference();
+
+        FirebaseUser user = mAuth.getInstance().getCurrentUser();
+        myRef.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>(){};
+                DiscTacks   = dataSnapshot.child("Tasks").getValue(t);
+
+                updateUI();
+
+            }
+
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         DatabaseReference ref = db.getReference("message"); // Key
         ref.setValue("This is a test message"); // Value
         return view;
+    }
+    private void updateUI() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(),android.R.layout.activity_list_item, DiscTacks);
+        ListUserTask.setAdapter(adapter);
     }
 }
